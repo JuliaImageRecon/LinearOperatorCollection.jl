ae5fd8c77ece312b339b3d3b8c7d4636788fff15
nHackel <nHackel@users.noreply.github.com>
2024-04-11 13:43:33 +0200

    Added S kwarg to WaveletOp

--- ext/LinearOperatorWaveletExt/WaveletOp.jl
+++ ext/LinearOperatorWaveletExt/WaveletOp.jl
@@ -10,10 +10,10 @@ a given input array.
 * `shape`                 - size of the Array to transform
 * (`wt=wavelet(WT.db2)`)  - Wavelet to apply
 """
-function LinearOperatorCollection.WaveletOp(::Type{T}; shape::Tuple, wt=wavelet(WT.db2)) where T <: Number
+function LinearOperatorCollection.WaveletOp(::Type{T}; shape::Tuple, wt=wavelet(WT.db2), S = Vector{T}) where T <: Number
   shape = filter(x-> x != 1, shape) # Drop dimension with 1
   return LinearOperator(T, prod(shape), prod(shape), false, false
             , (res,x)->dwt!(reshape(res,shape), reshape(x,shape), wt)
             , nothing
-            , (res,x)->idwt!(reshape(res,shape), reshape(x,shape), wt) )
+            , (res,x)->idwt!(reshape(res,shape), reshape(x,shape), wt), S = S)
 end
